3ab7e6e8a27c1d17ab29285932cba6d315b55bc1
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2025-06-21 23:43:22 +0500

    feat: all pagers

--- src/widgets/layout/ui/header.tsx
+++ src/widgets/layout/ui/header.tsx
@@ -4,7 +4,7 @@ import { AddTon } from '@/features/add-ton';
 
 export const Header = () => {
   return (
-    <header className="fixed w-full top-0 left-0 bg-dark-blue-50 ">
+    <header className="fixed w-full top-safe-content-top left-0 bg-dark-blue-50 ">
       <div className="min-h-15 px-6 py-3 grid place-items-end grid-cols-[auto_1fr] gap-2">
         <ConnectWalletTon />
 
