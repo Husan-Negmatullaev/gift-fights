37db30afff5009f180060726bb9c5ef93bc7ee17
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2025-06-23 17:00:10 +0500

    fix: fix nav

--- src/widgets/layout/ui/nav-bar.tsx
+++ src/widgets/layout/ui/nav-bar.tsx
@@ -1,48 +1,48 @@
-import type { IconNamesType } from '@/shared/types/icon-types';
-import { Icons } from '@/shared/ui/icons/icons';
-import clsx from 'clsx';
-import { NavLink, type NavLinkRenderProps } from 'react-router';
+import type { IconNamesType } from "@/shared/types/icon-types";
+import { Icons } from "@/shared/ui/icons/icons";
+import clsx from "clsx";
+import { NavLink, type NavLinkRenderProps } from "react-router";
 
 const navList: Array<{ icon: IconNamesType; text: string; path: string }> = [
   {
-    icon: 'trophy',
-    text: 'Лидеры',
-    path: '/leaders',
+    icon: "trophy",
+    text: "Лидеры",
+    path: "/leaders",
   },
   {
-    path: '/spin',
-    icon: 'rocket',
-    text: 'Играть',
+    path: "/spin",
+    icon: "rocket",
+    text: "Играть",
   },
   {
-    path: '/',
-    icon: 'home',
-    text: 'Главная',
+    path: "/",
+    icon: "home",
+    text: "Главная",
   },
   {
-    icon: 'box',
-    path: '/inventory',
-    text: 'Инвентарь',
+    icon: "box",
+    path: "/inventory",
+    text: "Инвентарь",
   },
   {
-    icon: 'user',
-    path: '/profile',
-    text: 'Профиль',
+    icon: "user",
+    path: "/profile",
+    text: "Профиль",
   },
 ];
 
 export const NavBar = () => {
   const setNavLinkClassName = (props: NavLinkRenderProps) => {
     return clsx(
-      'min-h-17 rounded-2.5 grid place-items-center transition-colors',
-      !props.isActive && 'text-white',
-      props.isActive && 'text-blue bg-dark-blue-150',
+      "min-h-17 rounded-2.5 grid place-items-center transition-colors",
+      !props.isActive && "text-white",
+      props.isActive && "text-blue bg-dark-blue-150",
     );
   };
 
   return (
     <nav className="grid items-center justify-center fixed left-0 bottom-0 min-h-21 w-full bg-dark-blue-50 py-2">
-      <ul className="grid grid-flow-col auto-cols-[68px] gap-6 pb-safe-area-bottom">
+      <ul className="grid grid-flow-col auto-cols-[68px] gap-2 pb-safe-area-bottom justify-between">
         {navList.map((item) => (
           <li key={item.path}>
             <NavLink to={item.path} className={setNavLinkClassName}>
