7664fa137d0ffbedf1fd2fd6f9dd1e1114abb66e
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2025-06-24 21:36:24 +0500

    chore: add retrieveLaunchParams()

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,17 @@
-import { initTelegram } from '@/entities/telegram';
-import { AppRouterProvider } from './providers/router-provider/ui/app-router-provider';
-import './styles/index.css';
-import ReactModal from 'react-modal';
+import { initTelegram } from "@/entities/telegram";
+import { AppRouterProvider } from "./providers/router-provider/ui/app-router-provider";
+import "./styles/index.css";
+import ReactModal from "react-modal";
+import { retrieveLaunchParams } from "@telegram-apps/sdk-react";
 
 initTelegram();
 
 function App() {
-  // useEffect(() => initTelegram(), []);
+  console.log(retrieveLaunchParams());
 
   return <AppRouterProvider />;
 }
 
 export default App;
 
-ReactModal.setAppElement('#root');
+ReactModal.setAppElement("#root");
