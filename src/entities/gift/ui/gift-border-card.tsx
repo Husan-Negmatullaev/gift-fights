da442a74331df21e8e9dc473ca6d2de6501abcc5
droggeljug <ibreatheyourlife@gmail.com>
2025-07-15 19:45:53 +0300

    fixes

--- src/entities/gift/ui/gift-border-card.tsx
+++ src/entities/gift/ui/gift-border-card.tsx
@@ -1,60 +1,80 @@
-import { Icons } from '@/shared/ui/icons/icons';
-import clsx from 'clsx';
-import type { GiftSizesType } from '../model/types/gift-types';
-import { GIFT_SIZES } from '../constants/gift-border-sizes-constants';
-import { TouchableLottie } from '@/shared/components/lottie/touchable-lottie';
-import { LoadableLottie } from '@/shared/components/lottie/loadable-lottie';
+import { LoadableLottie } from "@/shared/components/lottie/loadable-lottie";
+import { TouchableLottie } from "@/shared/components/lottie/touchable-lottie";
+import { Icons } from "@/shared/ui/icons/icons";
+import clsx from "clsx";
+import { GIFT_SIZES } from "../constants/gift-border-sizes-constants";
+import type { GiftSizesType } from "../model/types/gift-types";
 
 type GiftBorderCardProps = {
-  className?: string;
-  size: GiftSizesType;
+	className?: string;
+	size: GiftSizesType;
 
-  slug: string;
-  title: string;
-  price: number;
+	slug: string;
+	title: string;
+	price: number;
+	// status: string;
 };
 
 export const GiftBorderCard = (props: GiftBorderCardProps) => {
-  const { size, className, slug, title, price } = props;
+	const { size, className, slug, title, price } = props;
 
-  return (
-    <article
-      className={clsx(
-        className,
-        GIFT_SIZES[size].card,
-        'bg-dark-blue-50 text-white rounded-four',
-      )}>
-      <div
-        className={clsx(
-          GIFT_SIZES[size].image,
-          'pb-[95%] relative overflow-hidden rounded-four',
-        )}>
-        <LoadableLottie slug={slug}>
-          {(animationData) => (
-            <TouchableLottie
-              animation={animationData}
-              className="absolute size-full inset-0 object-cover overflow-hidden rounded-four"
-            />
-          )}
-        </LoadableLottie>
-      </div>
-      <header
-        className={clsx(
-          GIFT_SIZES[size].title,
-          GIFT_SIZES[size].header,
-          'flex items-center justify-between font-medium',
-        )}>
-        <h5>{title}</h5>
-        {/* <p>#{slug}</p> */}
-      </header>
-      <div
-        className={clsx(
-          GIFT_SIZES[size].button,
-          'font-medium flex items-center justify-center bg-blue w-full',
-        )}>
-        <Icons name="ton" className="size-2.5" />
-        <span>{price}</span>
-      </div>
-    </article>
-  );
+	if (status === "Withdrawing") {
+		return (
+			<article
+				className={clsx(
+					className,
+					"bg-dark-blue-50 text-white rounded-four relative min-h-[100px]",
+				)}
+			>
+				<div className="absolute inset-0 flex items-center justify-center">
+					<Icons name="loader" className="animate-spin size-8" />
+				</div>
+			</article>
+		);
+	}
+
+	return (
+		<article
+			className={clsx(
+				className,
+				GIFT_SIZES[size].card,
+				"bg-dark-blue-50 text-white rounded-four",
+			)}
+		>
+			<div
+				className={clsx(
+					GIFT_SIZES[size].image,
+					"pb-[95%] relative overflow-hidden rounded-four",
+				)}
+			>
+				<LoadableLottie slug={slug}>
+					{(animationData) => (
+						<TouchableLottie
+							animation={animationData}
+							className="absolute size-full inset-0 object-cover overflow-hidden rounded-four"
+						/>
+					)}
+				</LoadableLottie>
+			</div>
+			<header
+				className={clsx(
+					GIFT_SIZES[size].title,
+					GIFT_SIZES[size].header,
+					"flex items-center justify-between font-medium",
+				)}
+			>
+				<h5>{title}</h5>
+				{/* <p>#{slug}</p> */}
+			</header>
+			<div
+				className={clsx(
+					GIFT_SIZES[size].button,
+					"font-medium flex items-center justify-center bg-blue w-full",
+				)}
+			>
+				<Icons name="ton" className="size-2.5" />
+				<span>{price}</span>
+			</div>
+		</article>
+	);
 };
--- src/shared/constants/auth-token-constants.ts
+++ src/shared/constants/auth-token-constants.ts
@@ -1,5 +1,5 @@
 let MOCK_TELEGRAM_AUTH =
-  "query_id=AAHTmpAvAAAAANOakC9qsg2t&user=%7B%22id%22%3A798005971%2C%22first_name%22%3A%22HUSAN%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22Husan203%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FRDGUIBFtIMJxlvGNI_Q5WUJj7w9Fo5lLre75kJq0kyI.svg%22%7D&auth_date=1752331591&signature=LlRUPfO7dYEzzyWQNDJ4mIMjatOQuKVW4IGshRrO7e4bsMCDXEyeMAVyfNSb8LFIQ0MYfufumUK6jwAQbiK4Ag&hash=eda526a7406eff6603a0eb0930c5cb24649122800ed56b35befab5044581fc60";
+  "query_id=AAF-IPwYAAAAAH4g_BjPC6Mm&user=%7B%22id%22%3A419176574%2C%22first_name%22%3A%22droggeljug%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22droggeljug%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FGLZY3ED6eA8-rClEA--rwC7aiSt28gr5r38Uyy32UUw.svg%22%7D&auth_date=1752492124&signature=ca1ZpGbdqGokvlKp7tQWFw43bM0_P8No9KywyJsOfNkv7rcX2thHlzEoUpTFavFv7_GFNEfox_08a4C6yQ_wDQ&hash=24485577da64eddcae7d14e23511bbbe95c8909b7e5eac5b5bdcb6c1f54bd4f6";
 
 // if (confirm("IS AX")) {
 //   MOCK_TELEGRAM_AUTH =
