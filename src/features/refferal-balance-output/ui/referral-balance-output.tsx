55ca4afda97b285a6b72ac6e4f632036cf2aaad0
Хусан Нематуллаев <husannegmatullaev21@gmail.com>
2025-08-06 15:05:42 +0500

    fix: fix

--- src/features/refferal-balance-output/ui/referral-balance-output.tsx
+++ src/features/refferal-balance-output/ui/referral-balance-output.tsx
@@ -1,154 +1,150 @@
-import { useProfileContext } from "@/entities/profile";
-import { useTelegram } from "@/entities/telegram";
-import { useCreateWithdrawBonus } from "@/entities/user";
-import { useNavigationContext } from "@/shared/contexts/navigation-context";
-import { useToast } from "@/shared/hooks/use-toast";
-import { Icons } from "@/shared/ui/icons/icons";
-import { Modal } from "@/shared/ui/modal/modal";
-import { useCallback, useState } from "react";
+import { useProfileContext } from '@/entities/profile';
+import { useTelegram } from '@/entities/telegram';
+import { useCreateWithdrawBonus } from '@/entities/user';
+import { useNavigationContext } from '@/shared/contexts/navigation-context';
+import { useToast } from '@/shared/hooks/use-toast';
+import { Icons } from '@/shared/ui/icons/icons';
+import { Modal } from '@/shared/ui/modal/modal';
+import { useCallback, useState } from 'react';
 
 export const ReferralBalanceOutput = () => {
-	const { profile, refetch } = useProfileContext();
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [withdrawAmount, setWithdrawAmount] = useState("");
-	const { showSuccess, showError } = useToast();
-	const { createWithdrawBonus, loading } = useCreateWithdrawBonus();
-	const { hideNavBar, showNavBar } = useNavigationContext();
+  const { profile, refetch } = useProfileContext();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [withdrawAmount, setWithdrawAmount] = useState('');
+  const { showSuccess, showError } = useToast();
+  const { createWithdrawBonus, loading } = useCreateWithdrawBonus();
+  const { hideNavBar, showNavBar } = useNavigationContext();
 
-	const handleToggleModal = () => {
-		setIsModalOpen((prev) => !prev);
-	};
-	const tg = useTelegram();
-	const handleInputFocus = () => {
-		if (tg.platform !== "android" && tg.platform !== "ios") return;
-		hideNavBar();
-		// Scroll down by 20px when input is focused with delay
-		setTimeout(() => {
-			window.scrollBy({
-				top: 30,
-				behavior: "smooth",
-			});
-		}, 200); // 300ms delay
-	};
+  const handleToggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+  const tg = useTelegram();
+  const handleInputFocus = () => {
+    if (tg.platform !== 'android' && tg.platform !== 'ios') return;
+    hideNavBar();
+    // Scroll down by 20px when input is focused with delay
+    setTimeout(() => {
+      window.scrollBy({
+        top: 30,
+        behavior: 'smooth',
+      });
+    }, 200); // 300ms delay
+  };
 
-	const handleInputBlur = () => {
-		if (tg.platform !== "android" && tg.platform !== "ios") return;
-		showNavBar();
-	};
+  const handleInputBlur = () => {
+    if (tg.platform !== 'android' && tg.platform !== 'ios') return;
+    showNavBar();
+  };
 
-	const handleCreateWithdrawBonus = () => {
-		const amount = parseFloat(withdrawAmount) || 0;
-		createWithdrawBonus({ amount })
-			.then(() => {
-				refetch();
-				handleToggleModal();
-				showSuccess("Вывод реф. баланса успешно создан");
-			})
-			.catch(() => {
-				showError("Ошибка при выводе реф. баланса");
-			});
-	};
+  const handleCreateWithdrawBonus = () => {
+    const amount = parseFloat(withdrawAmount) || 0;
+    createWithdrawBonus({ amount })
+      .then(() => {
+        refetch();
+        handleToggleModal();
+        showSuccess('Вывод реф. баланса успешно создан');
+      })
+      .catch(() => {
+        showError('Ошибка при выводе реф. баланса');
+      });
+  };
 
-	const bonuses = profile?.bonuses || 0;
+  const bonuses = profile?.bonuses || 0;
 
-	function isTelegramWebApp() {
-		return typeof window !== "undefined" && !!window.Telegram?.WebApp;
-	}
+  function isTelegramWebApp() {
+    return typeof window !== 'undefined' && !!window.Telegram?.WebApp;
+  }
 
-	function canVibrate() {
-		return (
-			typeof window !== "undefined" &&
-			"vibrate" in window.navigator &&
-			typeof window.navigator.vibrate === "function"
-		);
-	}
+  function canVibrate() {
+    return (
+      typeof window !== 'undefined' &&
+      'vibrate' in window.navigator &&
+      typeof window.navigator.vibrate === 'function'
+    );
+  }
 
-	const onErrorWithdraw = useCallback(async () => {
-		console.log("onErrorWithdraw");
-		// Показываем toast уведомление
-		showError("Не хватает реф. баланса!");
+  const onErrorWithdraw = useCallback(async () => {
+    console.log('onErrorWithdraw');
+    // Показываем toast уведомление
+    showError('Не хватает реф. баланса!');
 
-		// Вибрация через Telegram WebApp
-		if (isTelegramWebApp() && window.Telegram.WebApp.HapticFeedback) {
-			window.Telegram.WebApp.HapticFeedback.notificationOccurred("success");
-		} else if (canVibrate()) {
-			// Fallback: обычная вибрация для браузера
-			window.navigator.vibrate(100);
-		}
-	}, [showError]);
+    // Вибрация через Telegram WebApp
+    if (isTelegramWebApp() && window.Telegram.WebApp.HapticFeedback) {
+      window.Telegram.WebApp.HapticFeedback.notificationOccurred('success');
+    } else if (canVibrate()) {
+      // Fallback: обычная вибрация для браузера
+      window.navigator.vibrate(100);
+    }
+  }, [showError]);
 
-	return (
-		<>
-			<article className="relative text-white mb-6 mt-4">
-				<div className="overflow-hidden relative">
-					<h2 className="font-bold text-lg mb-4">Ваша награда</h2>
-					<div className="flex items-center justify-between gap-4 relative rounded-[16px]">
-						<input
-							type="number"
-							value={withdrawAmount}
-							onChange={(e) => setWithdrawAmount(e.target.value)}
-							onFocus={handleInputFocus}
-							onBlur={handleInputBlur}
-							className="p-4 w-full focus:outline-none focus:ring-0  rounded-[16px] border border-[#FFFFFF66]"
-							placeholder="0.00 TON"
-						/>
-					</div>
-					<div className="flex justify-between mt-1">
-						<p className="text-sm text-[#A8A8A8]">Мин. сумма: {1} TON</p>
-						<p className="text-sm text-white">
-							Баланс: {profile?.balance || 0} TON
-						</p>
-					</div>
-					<button
-						type="button"
-						// disabled={bonuses === 0}
-						onClick={
-							bonuses === 0 || parseFloat(withdrawAmount) > bonuses
-								? onErrorWithdraw
-								: handleToggleModal
-						}
-						className="disabled:cursor-not-allowed cursor-pointer px-4 font-bold text-lg basis-25 shrink-0 bg-white text-black rounded-[16px] w-full py-[10px] mt-4"
-					>
-						Вывести
-					</button>
-				</div>
-			</article>
+  return (
+    <>
+      <article className="relative text-white mb-6 mt-4">
+        <div className="overflow-hidden relative">
+          <h2 className="font-bold text-lg mb-4">Ваша награда</h2>
+          <div className="flex items-center justify-between gap-4 relative rounded-[16px]">
+            <input
+              type="number"
+              value={withdrawAmount}
+              onChange={(e) => setWithdrawAmount(e.target.value)}
+              onFocus={handleInputFocus}
+              onBlur={handleInputBlur}
+              className="p-4 w-full focus:outline-none focus:ring-0  rounded-[16px] border border-[#FFFFFF66]"
+              placeholder="0.00 TON"
+            />
+          </div>
+          <div className="flex justify-between mt-1">
+            <p className="text-sm text-[#A8A8A8]">Мин. сумма: {1} TON</p>
+            <p className="text-sm text-white">
+              Баланс: {profile?.bonuses || 0} TON
+            </p>
+          </div>
+          <button
+            type="button"
+            // disabled={bonuses === 0}
+            onClick={
+              bonuses === 0 || parseFloat(withdrawAmount) > bonuses
+                ? onErrorWithdraw
+                : handleToggleModal
+            }
+            className="disabled:cursor-not-allowed cursor-pointer px-4 font-bold text-lg basis-25 shrink-0 bg-white text-black rounded-[16px] w-full py-[10px] mt-4">
+            Вывести
+          </button>
+        </div>
+      </article>
 
-			<Modal
-				open={isModalOpen}
-				onClose={handleToggleModal}
-				contentClassName="text-center"
-			>
-				<div className="grid gap-2.5 mb-7.5">
-					<span className="font-medium text-xl ">Вывод реф. баланса</span>
-					<p className="text-xs">Вы точно хотите вывести:</p>
-				</div>
+      <Modal
+        open={isModalOpen}
+        onClose={handleToggleModal}
+        contentClassName="text-center">
+        <div className="grid gap-2.5 mb-7.5">
+          <span className="font-medium text-xl ">Вывод реф. баланса</span>
+          <p className="text-xs">Вы точно хотите вывести:</p>
+        </div>
 
-				<h1 className="font-bold text-2.5xl mb-11 text-blue-100">
-					{withdrawAmount || "0"} TON
-				</h1>
+        <h1 className="font-bold text-2.5xl mb-11 text-blue-100">
+          {withdrawAmount || '0'} TON
+        </h1>
 
-				<div className="grid grid-cols-2 gap-2">
-					<button
-						disabled={loading}
-						className="cursor-pointer rounded-2xl min-h-12.5 bg-linear-180 from-red-250 from-0% to-red-300 to-100% shadow-[0px_0px_19.6px_0px_--alpha(var(--color-red-350)_/_50%)]"
-					>
-						Отмена
-					</button>
+        <div className="grid grid-cols-2 gap-2">
+          <button
+            disabled={loading}
+            className="cursor-pointer rounded-2xl min-h-12.5 bg-linear-180 from-red-250 from-0% to-red-300 to-100% shadow-[0px_0px_19.6px_0px_--alpha(var(--color-red-350)_/_50%)]">
+            Отмена
+          </button>
 
-					<button
-						disabled={loading}
-						onClick={handleCreateWithdrawBonus}
-						className="cursor-pointer rounded-2xl min-h-12.5 bg-linear-0 from-blue-50 from-0% to-blue-100 to-100% shadow-[0px_0px_19.6px_0px_--alpha(var(--color-blue-200)_/_50%)]"
-					>
-						{loading ? (
-							<Icons className="animate-pulse mx-auto" name="loader" />
-						) : (
-							"Вывести"
-						)}
-					</button>
-				</div>
-			</Modal>
-		</>
-	);
+          <button
+            disabled={loading}
+            onClick={handleCreateWithdrawBonus}
+            className="cursor-pointer rounded-2xl min-h-12.5 bg-linear-0 from-blue-50 from-0% to-blue-100 to-100% shadow-[0px_0px_19.6px_0px_--alpha(var(--color-blue-200)_/_50%)]">
+            {loading ? (
+              <Icons className="animate-pulse mx-auto" name="loader" />
+            ) : (
+              'Вывести'
+            )}
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
 };
--- src/shared/constants/auth-token-constants.ts
+++ src/shared/constants/auth-token-constants.ts
@@ -1,12 +1,12 @@
-let MOCK_TELEGRAM_AUTH =
+const MOCK_TELEGRAM_AUTH =
   'query_id=AAF-IPwYAAAAAH4g_Bgb60jR&user=%7B%22id%22%3A419176574%2C%22first_name%22%3A%22droggeljug%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22droggeljug%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FGLZY3ED6eA8-rClEA--rwC7aiSt28gr5r38Uyy32UUw.svg%22%7D&auth_date=1753883983&signature=2X7D8WDn0CeGk9J__rHdMXrD1aLQ0GcJHS7B12aobKf1se0lquH8Ll-q_zZ3VSeRb_QVnYaOVG8PPeQnfWwKDg&hash=185b5b2698d4236013795e73db63f928b8443fd679b900cbb0cba2da9be2ddda';
 
-if (import.meta.env.DEV) {
-  if (confirm('IS AX')) {
-    MOCK_TELEGRAM_AUTH =
-      'query_id=AAEvMR8XAwAAAC8xHxf6PG6m&user=%7B%22id%22%3A6830371119%2C%22first_name%22%3A%22%D0%A5%D1%83%D1%81%D0%B0%D0%BD%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22HusanAX%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FT5q0b4-Ep-Xr8PjtLnrrPBqPNACkKKBSgEMcCVTmIEdfukcdGoTPcUM1j5hcs3oG.svg%22%7D&auth_date=1752326567&signature=Eu3GKJYE9bhov_KZ20_jYEwSbsIwycg3RgN2FE0ztxfOL9KY0QAyQjONFzeQHRxaZzfYWX_TS3x2pclF8zfwBg&hash=9a767212dcf49e198cfaff418ecfa74b5c61086658d955b90f1daba4ecaea024';
-  }
-}
+// if (import.meta.env.DEV) {
+//   if (confirm('IS AX')) {
+//     MOCK_TELEGRAM_AUTH =
+//       'query_id=AAEvMR8XAwAAAC8xHxf6PG6m&user=%7B%22id%22%3A6830371119%2C%22first_name%22%3A%22%D0%A5%D1%83%D1%81%D0%B0%D0%BD%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22HusanAX%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FT5q0b4-Ep-Xr8PjtLnrrPBqPNACkKKBSgEMcCVTmIEdfukcdGoTPcUM1j5hcs3oG.svg%22%7D&auth_date=1752326567&signature=Eu3GKJYE9bhov_KZ20_jYEwSbsIwycg3RgN2FE0ztxfOL9KY0QAyQjONFzeQHRxaZzfYWX_TS3x2pclF8zfwBg&hash=9a767212dcf49e198cfaff418ecfa74b5c61086658d955b90f1daba4ecaea024';
+//   }
+// }
 
 const DEV_TELEGRAM_AUTH = import.meta.env.DEV
   ? MOCK_TELEGRAM_AUTH
